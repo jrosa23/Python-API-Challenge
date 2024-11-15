2319e28c254fc17f16b1564948c9b7a674de8bd7
jrosa23 <jeremyrosario@Js-M1-MBP.lan>
2024-11-15 01:09:17 -0500

    final commit

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "451a2208247a7e5366c7a87888da6261"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "cabe7495b45c49e795cff51948491f8d"
